181bc23d92c345867c12fbe13e02bbce19bb0604
Ubuntu User <ubuntuuser@ubuntuserver.com>
2025-02-08 20:22:42 +0100

    04 Intro to Rust Control Flow Statements & Conditional Expressions

--- README.md
+++ README.md
@@ -5,6 +5,9 @@
 Rust Programming Tutorial
 https://github.com/common1/rust_programming_tutorial.git
 
+The Rust Programming Language
+https://doc.rust-lang.org/book/title-page.html
+
 ## 01 Install the Rust Toolchain on an Ubuntu Linux Server
 https://www.youtube.com/watch?v=lTjGt17bQ3k&list=PLDbRgZ0OOEpUkWDGqp91ODn0dk7LPBAUL&index=1
 
@@ -17,6 +20,10 @@ https://www.youtube.com/watch?v=MJrBLTHJPCo&list=PLDbRgZ0OOEpUkWDGqp91ODn0dk7LPB
 ...
 
 ## 03 Rust Functions and Modules
+
+...
+
+## 04 Intro to Rust Control Flow Statements & Conditional Expressions
 
 ...
 
--- app/main.rs
+++ app/main.rs
@@ -6,11 +6,81 @@ fn main() {
     println!("Hello from Rust, Harrie smulders");
     // test_func();
     
-    let myresult = helpers::namehelpers::get_full_name("Harrie", "Smulders");
-    println!("Hello from {0}", myresult);
+    // let myresult = helpers::namehelpers::get_full_name("Harrie", "Smulders");
+    // println!("Hello from {0}", myresult);
 
-    let new_age = helpers::private_fns::get_age_plus_5(23);
-    println!("The new age is {0}", new_age);
+    // let new_age = helpers::private_fns::get_age_plus_5(23);
+    // println!("The new age is {0}", new_age);
+
+    // test_if();
+    // test_while();
+    // test_loop();
+    test_for();
+}
+
+fn test_for() {
+    let ages = [14, 18, 26, 35, 41];
+    let age_to_drive = 16i32;
+
+    for value in ages {
+        println!("The current value is {0}", value);
+        if value >= age_to_drive {
+            println!("You are old enough to drive!");
+        }
+        else {
+            println!("You need to wait a little bit more ...");
+        }
+    }
+}
+
+#[allow(dead_code)]
+fn test_loop() {
+    let mut x = 1;
+    loop {
+        println!("Hello from Rust!");
+        if x > 5 {
+            break;
+        }
+        x += 1;
+    }
+}
+
+#[allow(dead_code)]
+fn test_while() {
+    let age_to_drive = 16u8;
+
+    let mut current_age = 0u8;
+    while current_age < age_to_drive {
+        println!("Waiting ... ");
+        current_age += 1;
+        if current_age == 6 {
+            break;
+        }
+    }
+}
+
+#[allow(dead_code)]
+fn test_if() {
+    let age_to_drive = 16u8;
+
+    println!("Enter the person's age:");
+    let myinput = &mut String::from("");
+    std::io::stdin().read_line(myinput).unwrap();
+
+    // let age = myinput.replace("\n", "").parse::<u8>().unwrap();
+    let age = myinput.replace("\n", "").parse::<u8>().unwrap();
+    if age > age_to_drive{
+        println!("Issuing driver's license, because they are old enough");
+    }
+    else if age == 16 {
+        println!("You are just on the verge of being old enough! Wait one more year");
+    }
+    else {
+        println!("Wait a bit longer,m you eren't old enough for a driver's license");
+    }
+
+    let drivers_license = if age >= 16 { true } else { false };
+    println!("{}", drivers_license);
 }
 
 #[allow(dead_code)]
